1f82d9a5c3ea4643588081bd75575328c06fd9a9
IpsitaRoy22 <neharoy31@gmail.com>
2022-04-04 16:41:38 +0530

    Landing Page
    Landing Page

---
+++ src/components/homepage/Cards.js
@@ -0,0 +1,45 @@
+import './Homepage.css'
+function CardsEMS() {  
+ 
+    return (
+        <div class="main">
+        <div class="card-container">
+            <div class="card">
+                <div class="cardhead">
+                    Ernst & Young
+                </div>
+                <div class="cardvalue">
+                    Ernst & Young Global Limited, is a multinational network with headquarters in London, England.
+                    EY operates as a network of member firms which are structured as separate legal entities in a
+                    partnership, which has 312,250 employees in over 700 offices in more than 150 countries around
+                    the world.
+                </div>
+            </div>
+
+            <div class="card left">
+                <div class="cardhead">
+                    Employee Management System
+                </div>
+                <div class="cardvalue">
+                    An employee management system provides managers with insights into their workforce, and helps
+                    them to better plan and manage work hours to easily control labor costs and increase
+                    productivity.
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="cardhead">
+                    Capstone Project
+                </div>
+                <div class="cardvalue">
+                    Creating a web application where user can add employee information using CRUD(Create, Read,
+                    Update, Delete) operations.
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+  )};
+
+export default CardsEMS;
